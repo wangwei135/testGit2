0583c0eedc42b825f49f5b2fb5a1b35debd5dd8e
wangwei <wangwei38@corp.netease.com>
2021-10-25 20:41:41 +0800

    1to10

--- src/main/java/Main1.java
+++ src/main/java/Main1.java
@@ -1,6 +1,8 @@
 public class Main1 {
 
     public static void main(String[] args) {
-        System.out.println("444");
+        for(int i = 0 ; i < 10; i++) {
+            System.out.println(i);
+        }
     }
 }
